edf756b1a8fdc23a1abe51c291314a9bade6b5bd
Patrick Cloke <clokep@patrick.cloke.us>
2017-05-10 17:54:45 -0400

    Greatly simplify by putting all of a day's content in a single article.

--- mwcomposerfromhell/__init__.py
+++ mwcomposerfromhell/__init__.py
@@ -1,5 +1,5 @@
-from mwparserfromhell.definitions import get_html_tag, MARKUP_TO_HTML
-from mwparserfromhell.nodes import Comment, ExternalLink, Tag, Text, Wikilink
+from mwparserfromhell.definitions import MARKUP_TO_HTML
+from mwparserfromhell.nodes import ExternalLink, Tag, Template, Wikilink
 from mwparserfromhell.wikicode import Wikicode
 
 # The MARKUP_TO_HTML is missing a few things...this duck punches them in.
@@ -17,15 +17,19 @@ def format_wikicode(obj):
         return result
 
     if isinstance(obj, Tag):
-        # Get the HTML tag for this node.
-        tag = get_html_tag(obj.wiki_markup)
-
         # Convert all the children to HTML.
-        inner = ''.join(map(format_wikicode, obj.__children__()))
+        inner = u''.join(map(format_wikicode, obj.__children__()))
+
+        # Self closing tags don't need an end tag, this produces "broken" HTML,
+        # but readers should handle it fine.
+        if not obj.self_closing:
+            template = u'<{tag}>{inner}</{closing_tag}>'
+        else:
+            template = u'<{tag}>{inner}'
 
         # Create an HTML tag.
         # TODO Handle attributes.
-        return '<{}>{}</{}>'.format(tag, inner, tag)
+        return template.format(tag=obj.tag, inner=inner, closing_tag=obj.closing_tag)
 
     elif isinstance(obj, Wikilink):
         # Different text can be specified, or falls back to the title.
@@ -37,5 +41,11 @@ def format_wikicode(obj):
         text = obj.title or obj.url
         return u'<a href="{}">{}</a>'.format(obj.url, format_wikicode(text))
 
+    elif isinstance(obj, (list, tuple)):
+        # If the object is iterable, just handle each item separately.
+        result = u''
+        for node in obj:
+            result += format_wikicode(node)
+        return result
     else:
         return str(obj)
