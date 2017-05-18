0ab092bb5167f982cbc21950b797b413940e4d05
Patrick Cloke <clokep@patrick.cloke.us>
2017-05-18 17:32:35 -0400

    Switch back to generators.

--- mwcomposerfromhell/__init__.py
+++ mwcomposerfromhell/__init__.py
@@ -21,21 +21,18 @@ class WikicodeToHtmlComposer(object):
     https://en.wikipedia.org/wiki/Help:Wiki_markup
     """
     def __init__(self, base_url='https://en.wikipedia.org/wiki'):
-        self._base_url = base_url
-
-        self._article_url_format = base_url + '/{}'
+        # The base URL should be the root that articles sit in.
+        self._base_url = base_url.rstrip('/')
 
         self._wanted_lists = []
 
         # Track the currently open tags.
         self._stack = []
-        # The result of calling this.
-        self._parts = []
 
     def _get_url(self, title):
         """Given a page title, return a URL suitable for linking."""
         safe_title = url_quote(title.encode('utf-8'))
-        return self._article_url_format.format(safe_title)
+        return '{}/{}'.format(self._base_url, safe_title)
 
     def _close_stack(self, tag=None, raise_on_missing=True):
         """Close tags that are on the stack. It closes all tags until ``tag`` is found.
@@ -44,7 +41,7 @@ class WikicodeToHtmlComposer(object):
         # Close the entire stack.
         if tag is None:
             for current_tag in reversed(self._stack):
-                self._parts.append(u'</{}>'.format(current_tag))
+                yield u'</{}>'.format(current_tag)
             return
 
         # If a tag was given, close all tags behind it (in reverse order).
@@ -57,7 +54,7 @@ class WikicodeToHtmlComposer(object):
 
         while len(self._stack):
             current_tag = self._stack.pop()
-            self._parts.append(u'</{}>'.format(current_tag))
+            yield u'</{}>'.format(current_tag)
 
             if current_tag == tag:
                 break
@@ -78,12 +75,12 @@ class WikicodeToHtmlComposer(object):
 
             # Now close anything left in stack_lists.
             for node in reversed(stack_lists[i:]):
-                self._close_stack(node)
+                yield from self._close_stack(node)
 
             # Open anything in wanted_lists.
             for node in self._wanted_lists[i:]:
                 self._stack.append(node)
-                self._parts.append(u'<{}>'.format(node))
+                yield u'<{}>'.format(node)
 
             # Finally, open the list item.
             if self._wanted_lists[-1] == 'dl':
@@ -91,12 +88,12 @@ class WikicodeToHtmlComposer(object):
             else:
                 item_tag = 'li'
             self._stack.append(item_tag)
-            self._parts.append(u'<{}>'.format(item_tag))
+            yield u'<{}>'.format(item_tag)
 
             # Reset the list.
             self._wanted_lists = []
 
-        self._parts.append(part)
+        yield part
 
         # Certain tags get closed when there's a line break.
         if self._stack:
@@ -105,7 +102,7 @@ class WikicodeToHtmlComposer(object):
                     elements_to_close = ['li', 'ul', 'ol', 'dl', 'dt']
                     # Close an element in the stack.
                     if self._stack[-1] in elements_to_close:
-                        self._close_stack(self._stack[-1])
+                        yield from self._close_stack(self._stack[-1])
                 else:
                     break
 
@@ -113,7 +110,7 @@ class WikicodeToHtmlComposer(object):
         """Takes an object and returns a generator that will compose one more pieces of HTML."""
         if isinstance(obj, Wikicode):
             for node in obj.ifilter(recursive=False):
-                self._compose_parts(node)
+                yield from self._compose_parts(node)
 
         elif isinstance(obj, Tag):
             # Some tags require a parent tag to be open first, but get grouped
@@ -121,61 +118,61 @@ class WikicodeToHtmlComposer(object):
             if obj.wiki_markup == '*':
                 self._wanted_lists.append('ul')
                 # Don't allow a ul inside of a dl.
-                self._close_stack('dl', raise_on_missing=False)
+                yield from self._close_stack('dl', raise_on_missing=False)
             elif obj.wiki_markup == '#':
                 self._wanted_lists.append('ol')
                 # Don't allow a ul inside of a dl.
-                self._close_stack('dl', raise_on_missing=False)
+                yield from self._close_stack('dl', raise_on_missing=False)
             elif obj.wiki_markup == ';':
                 self._wanted_lists.append('dl')
                 # Don't allow dl instead ol or ul.
-                self._close_stack('ol', raise_on_missing=False)
-                self._close_stack('ul', raise_on_missing=False)
+                yield from self._close_stack('ol', raise_on_missing=False)
+                yield from self._close_stack('ul', raise_on_missing=False)
 
             else:
                 # Create an HTML tag.
                 # TODO Handle attributes.
-                self._add_part(u'<{}>'.format(obj.tag))
+                yield from self._add_part(u'<{}>'.format(obj.tag))
                 self._stack.append(obj.tag)
 
             for child in obj.__children__():
-                self._compose_parts(child)
+                yield from self._compose_parts(child)
 
             # Self closing tags don't need an end tag, this produces "broken"
             # HTML, but readers should handle it fine.
             if not obj.self_closing:
                 # Close this tag and any other open tags after it.
-                self._close_stack(obj.tag)
+                yield from self._close_stack(obj.tag)
 
         elif isinstance(obj, Wikilink):
             # Different text can be specified, or falls back to the title.
             text = obj.text or obj.title
             url = self._get_url(obj.title)
-            self._add_part(u'<a href="{}">'.format(url))
-            self._compose_parts(text)
-            self._add_part(u'</a>')
+            yield from self._add_part(u'<a href="{}">'.format(url))
+            yield from self._compose_parts(text)
+            yield from self._add_part(u'</a>')
 
         elif isinstance(obj, ExternalLink):
             # Different text can be specified, or falls back to the URL.
             text = obj.title or obj.url
-            self._add_part(u'<a href="{}">'.format(obj.url))
-            self._compose_parts(text)
-            self._add_part(u'</a>')
+            yield from self._add_part(u'<a href="{}">'.format(obj.url))
+            yield from self._compose_parts(text)
+            yield from self._add_part(u'</a>')
 
         elif isinstance(obj, Comment):
-            self._add_part(u'<!-- {} -->'.format(obj.contents))
+            yield from self._add_part(u'<!-- {} -->'.format(obj.contents))
 
         elif isinstance(obj, Text):
-            self._add_part(obj.value)
+            yield from self._add_part(obj.value)
 
         elif isinstance(obj, (HTMLEntity, Template)):
             # TODO
-            self._add_part(str(obj))
+            yield from self._add_part(str(obj))
 
         elif isinstance(obj, (list, tuple)):
             # If the object is iterable, just handle each item separately.
             for node in obj:
-                self._compose_parts(node)
+                yield from self._compose_parts(node)
 
         else:
             raise UnknownNode(u'Unknown node type: {}'.format(type(obj)))
@@ -184,9 +181,4 @@ class WikicodeToHtmlComposer(object):
         """Converts Wikicode or Node objects to HTML."""
         # TODO Add a guard that this can only be called once at a time.
 
-        self._compose_parts(obj)
-
-        # If any parts of the stack are still open, close them.
-        self._close_stack()
-
-        return u''.join(self._parts)
+        return u''.join(self._compose_parts(obj)) + u''.join(self._close_stack())
