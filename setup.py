7d353fa08d2f1117447fbbbc6641cdad804ecd5c
Patrick Cloke <clokep@patrick.cloke.us>
2018-10-24 12:18:45 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def long_description():
 
 setup(
     name='mwcomposerfromhell',
-    version="0.1",
+    version="0.2dev",
     packages=find_packages(),
     description='Convert the parsed MediaWiki wikicode (using mwparserfromhell) to HTML.',
     long_description=long_description(),
