c844de787d95b31b3e379d7a16082b3a850dc2f6
Patrick Cloke <clokep@patrick.cloke.us>
2019-06-26 08:49:35 -0400

    Start loading tests from a file.

--- tests/parser_tests/test_mediawiki.py
+++ tests/parser_tests/test_mediawiki.py
@@ -1,4 +1,4 @@
-from io import StringIO
+from pathlib import Path
 
 import mwparserfromhell
 
@@ -8,20 +8,18 @@ from . import MediaWikiParserTestCasesParser
 
 def pytest_generate_tests(metafunc):
     """Auto-generate test cases from parserTests.txt."""
-    parser = MediaWikiParserTestCasesParser(StringIO("""!! test
-Blank input
-!! wikitext
-!! html
-!! end
-"""))
-    parser.parse()
+    with open(Path(__file__).parent.joinpath('parserTests.txt')) as f:
+        parser = MediaWikiParserTestCasesParser(f)
+        parser.parse()
 
     argnames = ['wikitext', 'html']
+    # Ignore tests without HTML.
+    test_cases = list(filter(lambda t: 'html' in t, parser.test_cases))
     # Use the test name as the ID.
-    ids = list(map(lambda d: d['test'].strip(), parser.test_cases))
+    ids = list(map(lambda t: t['test'].strip(), test_cases))
     # Pull out the wikitext and expected HTML.
     # TODO Handle different HTML types.
-    argvalues = map(lambda d: (d['wikitext'], d['html']), parser.test_cases)
+    argvalues = map(lambda d: (d['wikitext'], d['html']), test_cases)
 
     metafunc.parametrize(argnames, argvalues, ids=ids)
 
