de343cec28ac1d5c98282d8eb9bded995e984fdd
Patrick Cloke <clokep@patrick.cloke.us>
2020-05-26 14:31:18 -0400

    Move table tests to a separate file.

--- tests/test_composer.py
+++ tests/test_composer.py
@@ -54,37 +54,6 @@ def test_entity_in_text():
     assert compose(wikicode) == '<p>&lt;test&gt; &amp;</p>'
 
 
-def test_table():
-    """Test an wiki table."""
-    content = """{|
-|-
-! Header 1 !! Header 2 !! Header 3
-|-
-| Example 1a || Example 2a || Example 3a
-|-
-| Example 1b || Example 2b || Example 3b
-|}"""
-    wikicode = mwparserfromhell.parse(content)
-    assert compose(wikicode) == """<table><tr><th> Header 1 </th><th> Header 2 </th><th> Header 3
-</th></tr><tr><td> Example 1a </td><td> Example 2a </td><td> Example 3a
-</td></tr><tr><td> Example 1b </td><td> Example 2b </td><td> Example 3b
-</td></tr></table>"""
-
-
-def test_table_class():
-    """Test an wiki table."""
-    content = """{| class="wikitable"
-|-
-! Header 1 !! Header 2
-|-
-| Example 1 || Example 2
-|}"""
-    wikicode = mwparserfromhell.parse(content)
-    assert compose(wikicode) == """<table class="wikitable"><tr><th> Header 1 </th><th> Header 2
-</th></tr><tr><td> Example 1 </td><td> Example 2
-</td></tr></table>"""
-
-
 def test_unknown_node():
     """An unknown node type should raise an error."""
     with pytest.raises(UnknownNode):
