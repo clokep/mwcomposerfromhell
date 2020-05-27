cce09ad846ddcace86e2404eec423a662366ed5d
Patrick Cloke <clokep@patrick.cloke.us>
2020-05-27 11:55:09 -0400

    Properly canonicalize article titles.

--- mwcomposerfromhell/namespace.py
+++ mwcomposerfromhell/namespace.py
@@ -1,15 +1,111 @@
+from collections import namedtuple
+import html
+import re
 from typing import Any, Dict, Tuple
+from urllib.parse import unquote
+
+
+MULTIPLE_UNDERSCORES = re.compile(r'_+')
 
 
 class ArticleNotFound(Exception):
     """The article was not found."""
 
 
-def _clean_name(key: str) -> str:
-    """MediaWiki treats the first character of article names as case-insensitive."""
+class InvalidTitle(Exception):
+    """The given title cannot point to an article."""
+
+
+CanonicalTitle = namedtuple('CanonicalTitle', ('namespace', 'title', 'interwiki'))
+
+
+def _normalize_spaces(key: str) -> str:
+    """Spaces and turned to underscores, multiple get combined, and stripped from the beginning and end."""
+    # Convert spaces to underscores.
+    key = key.replace(' ', '_')
+    # Replace strings of underscores with a single underscore.
+    key = MULTIPLE_UNDERSCORES.sub('_', key)
+    # Remove all underscores from the start and end.
+    return key.strip('_')
+
+
+def _normalize_namespace(key: str) -> str:
+    """MediaWiki treats the first character of namespaces as upper-case and the rest as lower-case."""
+    if not key:
+        return ''
+    key = _normalize_spaces(key)
+    return key[0].upper() + key[1:].lower()
+
+
+def _normalize_title(key: str) -> str:
+    """MediaWiki treats the first character of article names as upper-case."""
     if not key:  # Empty string
-        return key
-    return key[0].lower() + key[1:]
+        return ''
+    key = _normalize_spaces(key)
+    return key[0].upper() + key[1:]
+
+
+def canonicalize_title(title: str) -> str:
+    """
+    Generate the canonical form of a title.
+
+    See https://en.wikipedia.org/wiki/Help:Link#Conversion_to_canonical_form
+
+    TODO Handle anonymous user pages.
+    """
+    # HTML entities and percent encoded characters get converted to their raw
+    # character.
+    title = html.unescape(unquote(title))
+
+    # Convert spaces to underscores.
+    title = title.replace(' ', '_')
+    # Replace strings of underscores with a single underscore.
+    title = MULTIPLE_UNDERSCORES.sub('_', title)
+    # Remove all underscores from the start and end.
+    title = title.strip('_')
+
+    # The parts are separate by colons.
+    parts = title.split(':')
+    has_interwiki = title[0] == ':'
+
+    # Generally only 1 - 3 parts are expected (interwiki, namespace, and title).
+    num_parts = len(parts)
+    if num_parts >= 4:
+        if has_interwiki:
+            interwiki = parts[1]
+            parts = parts[2:]
+        else:
+            interwiki = ''
+        namespace = parts[0]
+        title = ':'.join(parts[1:])
+    elif num_parts == 3:
+        if has_interwiki:
+            _, interwiki, title = parts
+            namespace = ''
+        else:
+            interwiki = ''
+            namespace = parts[0]
+            title = ':'.join(parts[1:])
+    elif num_parts == 2:
+        # In this case an interwiki link cannot be given.
+        interwiki = ''
+        namespace, title = parts
+        # Not providing a title in this case is invalid, e.g. [[Foo:]] is not a
+        # valid link.
+        if not title:
+            raise InvalidTitle(title)
+    else:
+        # No colons, it is just a page title.
+        interwiki = ''
+        namespace = ''
+        title = parts[0]
+
+    # Each of the pieces again has and starting / trailing underscores removed.
+    interwiki = interwiki.strip('_')
+    namespace = namespace.strip('_')
+    title = title.strip('_')
+
+    return CanonicalTitle(_normalize_namespace(namespace), _normalize_title(title), interwiki)
 
 
 class Namespace:
@@ -26,14 +122,14 @@ class Namespace:
         else:
             # Fix the names of any incoming articles.
             self._articles = {
-                _clean_name(name): article for name, article in articles.items()
+                _normalize_title(name): article for name, article in articles.items()
             }
 
     def __getitem__(self, key: str) -> Any:
-        return self._articles[_clean_name(key)]
+        return self._articles[_normalize_title(key)]
 
     def __setitem__(self, key: str, value: Any) -> Any:
-        self._articles[_clean_name(key)] = value
+        self._articles[_normalize_title(key)] = value
         return value
 
 
@@ -42,7 +138,7 @@ class ArticleResolver:
         self._namespaces = {}  # Dict[str, Namespace]
 
     def add_namespace(self, name: str, namespace: Namespace) -> None:
-        self._namespaces[_clean_name(name)] = namespace
+        self._namespaces[_normalize_namespace(name)] = namespace
 
     def resolve_article(self, name: str, default_namespace: str) -> Tuple[str, str]:
         """
@@ -51,12 +147,11 @@ class ArticleResolver:
         :param name: The name of the article to find.
         :param default_namespace: The namespace to use, if one is not provided.
         """
-        parts = name.split(':', 1)
-        # If there was not a colon, it is in the main namespace, which is a
-        # blank string.
-        if len(parts) == 1:
-            return default_namespace, parts[0]
-        return tuple(parts)  # type: ignore
+        canonical_title = canonicalize_title(name)
+        # Use the default namespace if one was not parsed.
+        if not canonical_title.namespace and default_namespace:
+            return CanonicalTitle(default_namespace, *canonical_title[1:])
+        return canonical_title
 
     def get_article(self, name: str, default_namespace: str = ''):
         """
@@ -65,9 +160,9 @@ class ArticleResolver:
         :param name: The name of the article to find.
         :param default_namespace: The namespace to use, if one is not provided.
         """
-        namespace, name = self.resolve_article(name, default_namespace)
+        canonical_title = self.resolve_article(name, default_namespace)
 
         try:
-            return self._namespaces[_clean_name(namespace)][name]
+            return self._namespaces[canonical_title.namespace][canonical_title.title]
         except KeyError:
-            raise ArticleNotFound(namespace, name)
+            raise ArticleNotFound(canonical_title)
--- tests/test_namespaces.py
+++ tests/test_namespaces.py
@@ -1,7 +1,12 @@
 import mwparserfromhell
 import pytest
 
-from mwcomposerfromhell.namespace import ArticleNotFound, ArticleResolver, Namespace
+from mwcomposerfromhell.namespace import (
+    ArticleNotFound,
+    ArticleResolver,
+    canonicalize_title,
+    Namespace,
+)
 
 
 @pytest.fixture
@@ -58,21 +63,104 @@ def test_shadowed(resolver):
     assert resolver.get_article('Foo') != resolver.get_article('Template:Foo')
 
 
-def test_case_insensitivity(resolver):
-    """The first letter is case-insensitive."""
+def test_case_sensitivity(resolver):
+    """The first letter is automatically capitalized."""
     assert resolver.get_article('Main') == resolver.get_article('main')
 
     # The case of other letters matters.
     with pytest.raises(ArticleNotFound):
         resolver.get_article('MAIN')
 
-    # The same applies to namespaces.
+    # The case of namespaces is normalized.
     foo = resolver.get_article('Template:Foo')
     assert foo == resolver.get_article('Template:Foo')
     assert foo == resolver.get_article('template:Foo')
     assert foo == resolver.get_article('template:foo')
+    assert foo == resolver.get_article('TEMPLATE:Foo')
 
     with pytest.raises(ArticleNotFound):
-        resolver.get_article('TEMPLATE:Foo')
-    with pytest.raises(ArticleNotFound):
         resolver.get_article('Template:FOO')
+
+
+@pytest.mark.parametrize(('input', ), [
+    # Standard form.
+    ('Main page', ),
+    # First character gets upper-cased.
+    ('main page', ),
+    # Underscores.
+    ('Main_page', ),
+    # Extra underscores.
+    ('__Main_page___', ),
+    # Extra spaces.
+    ('  Main page   ', ),
+    # Namespace.
+    (':main page', ),
+])
+def test_canonicalize(input):
+    """Test canonicalizing a domain in the main namespace"""
+    assert canonicalize_title(input) == ('', 'Main_page', '')
+
+
+@pytest.mark.parametrize(('input', ), [
+    # Standard form.
+    ('Template:Foo bar', ),
+    # First character gets upper-cased.
+    ('template:foo bar', ),
+    # Underscores.
+    ('template:foo_bar', ),
+    # Extra underscores.
+    ('__template__:__foo_bar___', ),
+    # Extra spaces.
+    ('  template   :  foo bar   ', ),
+])
+def test_canonicalize_with_namespace(input):
+    """Test canonicalizing a domain in the main namespace"""
+    assert canonicalize_title(input) == ('Template', 'Foo_bar', '')
+
+
+@pytest.mark.parametrize(('input', ), [
+    # Standard form.
+    (':en:Foo bar', ),
+    # Underscores.
+    (':en:foo_bar', ),
+    # Extra underscores.
+    ('__:__en__:__foo_bar___', ),
+    # Extra spaces.
+    ('  :  en   :  foo bar   ', ),
+])
+def test_canonicalize_with_interwiki(input):
+    """Test canonicalizing a domain in the main namespace"""
+    assert canonicalize_title(input) == ('', 'Foo_bar', 'en')
+
+
+
+@pytest.mark.parametrize(('input', ), [
+    # Standard form.
+    (':en:Template:Foo bar', ),
+    # Underscores.
+    (':en:Template:foo_bar', ),
+    # Extra underscores.
+    ('__:__en__:__Template__:__foo_bar___', ),
+    # Extra spaces.
+    ('  :  en   :  Template  :  foo bar   ', ),
+])
+def test_canonicalize_with_namespace_and_interwiki(input):
+    """Test canonicalizing a domain in the main namespace"""
+    assert canonicalize_title(input) == ('Template', 'Foo_bar', 'en')
+
+
+@pytest.mark.parametrize(('input', 'expected'), [
+    # HTML entities.
+    ('d&eacute;partement', 'Département'),
+    # Percent-encoded.
+    ('%40', '@'),
+    # Percent and HTML encoded! (%26 = &)
+    ('Foo%26amp;', 'Foo&'),
+    # Encoded colons get unescaped first.
+    ('%3Ade%3AFoo', ('', 'Foo', 'de')),
+])
+def test_canonicalize_escape(input, expected):
+    """Test canonicalizing a domain in the main namespace"""
+    if isinstance(expected, str):
+        expected = ('', expected, '')
+    assert canonicalize_title(input) == expected
