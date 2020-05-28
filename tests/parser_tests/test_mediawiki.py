e37e36c79d06a5b682458d2bd38964f04018458f
Patrick Cloke <clokep@patrick.cloke.us>
2020-05-28 14:08:02 -0400

    Try matching parsoid output if that's all that exists.

--- tests/parser_tests/test_mediawiki.py
+++ tests/parser_tests/test_mediawiki.py
@@ -18,7 +18,7 @@ ONLY_RUN_WHITELIST = False
 
 # Some tests have a standard HTML output, while others differ based on the
 # parser. Prefer the standard output, then the PHP parser's.
-PREFERRED_HTML = ('html', 'html/*', 'html/php', 'html/php+tidy')
+PREFERRED_HTML = ('html', 'html/*', 'html/php', 'html/php+tidy', 'html/parsoid')
 # A known object.
 _SENTINEL = object()
 
