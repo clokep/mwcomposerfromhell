f8d754d05214f0a7d1c93627236a208f9da6607d
Patrick Cloke <clokep@patrick.cloke.us>
2020-06-04 15:16:08 -0400

    Add some tests for error conditions of magic words.

--- tests/test_magic_words.py
+++ tests/test_magic_words.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import mwparserfromhell
+import pytest
 
 from mwcomposerfromhell import compose
 from tests import patch_datetime
@@ -9,35 +10,48 @@ from tests import patch_datetime
 patch_datetime_fixture = patch_datetime(datetime(2001, 8, 3, 9, 2, 3))
 
 
-def pytest_generate_tests(metafunc):
-    argnames = ('magic_word', 'output')
-    argvalues = [
-        ('YEAR', '2001'),
-        ('MONTH', '08'),
-        ('MONTH1', '8'),
-        ('MONTHNAME', 'August'),
-        ('MONTHABBREV', 'Aug'),
-        ('DAY', '3'),
-        ('DAY2', '03'),
-        ('DOW', '5'),
-        ('DAYNAME', 'Friday'),
-        ('TIME', '09:02'),
-        ('HOUR', '09'),
-        ('WEEK', '32'),
-        ('TIMESTAMP', '20010803090203'),
-    ]
+_argvalues = [
+    ('YEAR', '2001'),
+    ('MONTH', '08'),
+    ('MONTH1', '8'),
+    ('MONTHNAME', 'August'),
+    ('MONTHABBREV', 'Aug'),
+    ('DAY', '3'),
+    ('DAY2', '03'),
+    ('DOW', '5'),
+    ('DAYNAME', 'Friday'),
+    ('TIME', '09:02'),
+    ('HOUR', '09'),
+    ('WEEK', '32'),
+    ('TIMESTAMP', '20010803090203'),
+]
+
+
+@pytest.mark.parametrize(
+    ('magic_word', 'output'),
     # Generate both a current and local version of the tests.
-    argvalues = [
-        ('CURRENT' + magic_word, value) for (magic_word, value) in argvalues
+    [
+        ('CURRENT' + magic_word, value) for (magic_word, value) in _argvalues
     ] + [
-        ('LOCAL' + magic_word, value) for (magic_word, value) in argvalues
+        ('LOCAL' + magic_word, value) for (magic_word, value) in _argvalues
     ]
-
-    metafunc.parametrize(argnames, argvalues, ids=[v[0] for v in argvalues])
-
-
+)
 def test_magic_words(magic_word, output):
     """Test that simple formatting works."""
     content = "{{%s}}" % magic_word
     wikicode = mwparserfromhell.parse(content)
     assert compose(wikicode) == '<p>%s</p>' % output
+
+
+def test_case_sensitivty():
+    """Magic words are case-sensitive."""
+    content = "{{currentyear}}"
+    wikicode = mwparserfromhell.parse(content)
+    assert compose(wikicode) == '<p>{{currentyear}}</p>'
+
+
+def test_params():
+    """Parameters get discarded."""
+    content = "{{CURRENTYEAR|foo}}"
+    wikicode = mwparserfromhell.parse(content)
+    assert compose(wikicode) == '<p>2001</p>'
